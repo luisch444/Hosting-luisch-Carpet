5c7fdafd983f2a7586c3c582b48e05d814420de8
luisch444 <luisch2005@gmail.com>
2025-08-18 22:40:13 +0100

    Updated to 1.21.7

--- src/main/java/xyz/luisch444/carpet/mixin/SquidEntityMixin.java
+++ src/main/java/xyz/luisch444/carpet/mixin/SquidEntityMixin.java
@@ -2,15 +2,15 @@ package xyz.luisch444.carpet.mixin;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
-import net.minecraft.entity.mob.WaterCreatureEntity;
 import net.minecraft.entity.passive.SquidEntity;
+import net.minecraft.entity.passive.WaterAnimalEntity;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import xyz.luisch444.carpet.HostingluischSettings;
 
 @Mixin(SquidEntity.class)
-public class SquidEntityMixin extends WaterCreatureEntity {
+public abstract class SquidEntityMixin extends WaterAnimalEntity {
     public SquidEntityMixin(EntityType<? extends SquidEntity> entityType, World world) {
         super(entityType, world);
     }
