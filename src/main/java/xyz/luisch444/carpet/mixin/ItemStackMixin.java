1903b7a6445e40e9657f8b742dee7e8e36a85a8b
luisch444 <luisch2005@gmail.com>
2026-08-10 14:11:15 +0100

    Updated 26.1

--- src/main/java/xyz/luisch444/carpet/mixin/ItemStackMixin.java
+++ src/main/java/xyz/luisch444/carpet/mixin/ItemStackMixin.java
@@ -3,6 +3,7 @@ package xyz.luisch444.carpet.mixin;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemStackView;
 import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -12,14 +13,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xyz.luisch444.carpet.HostingluischSettings;
 
 @Mixin(ItemStack.class)
-public abstract class ItemStackMixin {
+public abstract class ItemStackMixin implements ItemStackView {
     @Shadow
     public abstract Item getItem();
 
-    @Inject(method = "getMaxCount", at = @At("HEAD"), cancellable = true)
-    private void stackingItems(CallbackInfoReturnable<Integer> cir) {
+    @Override
+    public int getMaxCount() {
         if (HostingluischSettings.stackeableTotems && Items.TOTEM_OF_UNDYING.equals(this.getItem())){
-            cir.setReturnValue(HostingluischSettings.stackeableTotemsSize);
+            return HostingluischSettings.stackeableTotemsSize;
         }
+        return ItemStackView.super.getMaxCount();
     }
 }
